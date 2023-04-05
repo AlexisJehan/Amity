2305e6a0802bc93948bfa88832d1ebc9aeae6d64
Alexis <alexis.jehan@outlook.com>
2023-04-05 18:47:05 +0200

    Update framework.lang.php

--- system/framework/languages/framework.lang.php
+++ system/framework/languages/framework.lang.php
@@ -37,8 +37,33 @@
 
 	$dictionary['fr'] = array(
 
-		// Traduction dans plusieurs classes (Image, Json, MySQLDatabaseService, MySQLiDatabaseService, WebRequest)
+		// Traduction dans les classes :
+		// - « core/responses/Image »
+		// - « core/responses/Json »
+		// - « core/responses/Xml »
+		// - « services/LanguageService »
+		// - « services/database/MySQLDatabaseService »
+		// - « services/database/MySQLiDatabaseService »
+		// - « services/database/PDODatabaseService »
+		// - « services/database/SpecificMySQLDatabaseService »
+		// - « services/database/SpecificMySQLiDatabaseService »
+		// - « utils/WebRequest »
 		'"%s" extension is not available' => 'L\'extension « %s » n\'est pas disponible',
+
+		// Traduction dans les classes :
+		// - « core/responses/authentications/Authentication »
+		// - « core/responses/pages/errors/ErrorPage »
+		'Error' => 'Erreur',
+
+		// Traduction dans les classes :
+		// - « services/database/MySQLDatabaseService »
+		// - « services/database/MySQLiDatabaseService »
+		'Unavailable or unimplemented fetch method with flag "%s" using "%s" database service' => 'Méthode de récupération indisponible ou non implémentée pour la valeur « %s » en utilisant le service de base de données « %s »',
+
+		// Traduction dans les classes :
+		// - « utils/Logger »
+		// - « utils/cache/Cache »
+		'Unable to create "%s" directory' => 'Impossible de créer le dossier « %s »',
 
 		// Traduction dans la classe « core/FrontController »
 		'Responses forwards loop detected, unable to handle the request' => 'Forwards mutuels de réponses, impossible de traiter la requête',
@@ -84,28 +109,23 @@
 		'Service Unavailable' => 'Service indisponible',
 		'Gateway Time-out' => 'Intermédiaire hors-délai',
 		'HTTP Version not supported' => 'Version HTTP non supportée',
-
 		'Unable to send HTTP headers because some content has already been sent from "%s" on line %s' => 'Impossible d\'envoyer les entêtes HTTP car du contenu a déjà été retourné depuis « %s » à la ligne %s',
 		'The value "%s" is not valid for the HTTP protocol, it must match "HTTP/x.x"' => 'La valeur « %s » est invalide pour le protocole HTTP, elle doit correspondre à « HTTP/x.x »',
 		'The value "%s" is not valid for the HTTP status, it must be between 100 and 599' => 'La valeur « %s » est invalide pour le status HTTP, elle doit être comprise entre 100 et 599',
 		'A custom status message is expected for the "%s" HTTP status' => 'Un message personnalisé est attendu pour le status HTTP « %s »',
 
-		// Traduction dans la classe « core/responses/authentications/Authentication » / « core/responses/authentications/DigestAuthentication »
+		// Traduction dans la classe « core/responses/Image »
+		'The image "%s" is not a valid resource, it must be a "gd" resource' => 'L\'image « %s » n\'est pas une ressource valide, elle doit être une ressource de type « gd »',
+
+		// Traduction dans la classe « core/responses/authentications/Authentication »
 		'Username' => 'Nom d\'utilisateur',
 		'Success' => 'Succès',
 		'Failure' => 'Échec',
 		'Status' => 'Status',
 		'You need to provide a valid username and password.' => 'Un nom d\'utilisateur et un mot de passe valides sont attendus.',
 
-		// Traduction dans la classe « core/responses/Image »
-		'The image "%s" is not a valid resource, it must be a "gd" resource' => 'L\'image « %s » n\'est pas une ressource valide, elle doit être une ressource de type « gd »',
-
 		// Traduction dans la classe « core/responses/downloads/FileDownload »
 		'"%s" is not a valid file or it cannot be read' => '« %s » n\'est pas un fichier valide ou il ne peut être lu',
-
-		// Traduction dans la classe « core/responses/pages/errors/ErrorPage »
-		'Error' => 'Erreur',
-		'An error has occurred.' => 'Une erreur est survenue.',
 
 		// Traduction dans la classe « core/responses/pages/errors/Error403Page »
 		'You are not allowed to access this page.' => 'Vous ne pouvez pas accéder à cet emplacement.',
@@ -119,9 +139,11 @@
 		// Traduction dans la classe « core/responses/pages/errors/Error503Page »
 		'The website is currently unavailable due to maintenance works, please try again later.' => 'Le site n\'est pas disponible actuellement pour cause de maintenance, merci de retenter ultérieurement.',
 
+		// Traduction dans la classe « core/responses/pages/errors/ErrorPage »
+		'An error has occurred.' => 'Une erreur est survenue.',
+
 		// Traduction dans la classe « core/responses/redirects/DelayedRedirect »
-		'This location has moved. You will be automatically redirected to its new location in %s seconds. If you aren\'t forwarded to the new page, %sclick here%s.'
-		=> 'Cet emplacement a été déplacé. Vous allez être redirigé de manière automatique dans %s secondes. Si vous n\'êtes pas redirigé automatiquement, %scliquez ici%s.',
+		'This location has moved. You will be automatically redirected to its new location in %s seconds. If you aren\'t forwarded to the new page, %sclick here%s.' => 'Cet emplacement a été déplacé. Vous allez être redirigé de manière automatique dans %s secondes. Si vous n\'êtes pas redirigé automatiquement, %scliquez ici%s.',
 
 		// Traduction dans la classe « loaders/AbstractLoader »
 		'"%s" location does not exist' => 'L\'emplacement « %s » n\'existe pas',
@@ -145,39 +167,24 @@
 		'Unable to register hook "%s" because function "%s" does not exist' => 'Impossible d\'enregistrer le crochet « %s » car la fonction « %s » n\'existe pas',
 		'Unable to register hook "%s" because class "%s" does not exist' => 'Impossible d\'enregistrer le crochet « %s » car la classe « %s » n\'existe pas',
 		'Unable to register hook "%s" because class "%s" does not have method "%s"' => 'Impossible d\'enregistrer le crochet « %s » car la classe « %s » ne possède pas de méthode « %s »',
-		'Unable to register hook "%s" because class "%s" has method "%s" but it cannot be accessed'
-		=> 'Impossible d\'enregistrer le crochet « %s » car la classe « %s » possède bien la méthode « %s », mais cette dernière n\'est pas accessible',
+		'Unable to register hook "%s" because class "%s" has method "%s" but it cannot be accessed' => 'Impossible d\'enregistrer le crochet « %s » car la classe « %s » possède bien la méthode « %s », mais cette dernière n\'est pas accessible',
 		'Unable to register hook "%s" because object "%s" does not have method "%s"' => 'Impossible d\'enregistrer le crochet « %s » car l\'objet « %s » ne possède pas de méthode « %s »',
-		'Unable to register hook "%s" because object "%s" has method "%s" but it cannot be accessed'
-		=> 'Impossible d\'enregistrer le crochet « %s » car l\'objet « %s » possède bien la méthode « %s », mais cette dernière n\'est pas accessible',
+		'Unable to register hook "%s" because object "%s" has method "%s" but it cannot be accessed' => 'Impossible d\'enregistrer le crochet « %s » car l\'objet « %s » possède bien la méthode « %s », mais cette dernière n\'est pas accessible',
 		'Unable to register hook "%s" because "%s" does not refer to a class or an object' => 'Impossible d\'enregistrer le crochet « %s » car « %s » ne correspond ni à une classe ni à un objet',
 		'Unable to register hook "%s" because "%s" does not refer to a callable' => 'Impossible d\'enregistrer le crochet « %s » car « %s » ne correspond pas à une fonction de rappel',
 
 		// Traduction dans la classe « services/LanguageService »
 		'The value "%s" is not valid for the language' => 'La valeur « %s » n\'est pas valide pour la langue',
-		'Language' => 'Langue',
-		'String' => 'Chaîne',
 
 		// Traduction dans la classe « services/Service »
 		'The service "%s" has already been initialised' => 'Le service « %s » a déjà été initialisé',
 		'The service "%s" has not been created' => 'Le service « %s » n\'a pas été crée',
 
-		// Traduction dans la classe « services/database/MySQLDatabaseService » / « services/database/MySQLiDatabaseService »
-		'Unavailable or unimplemented fetch method with flag "%s" using "%s" database service'
-		=> 'Méthode de récupération indisponible ou non implémentée pour la valeur « %s » en utilisant le service de base de données « %s »',
-
-		// Traduction dans la classe « tools/WebRequest »
-		'cURL extension is required to use WebRequest class' => 'L\'extension cURL est requise pour utiliser le classe WebRequest',
-
 		// Traduction dans la classe « utils/DatabaseFactory »
 		'Unable to create the database service because "%s" is missing from the configuration' => 'Impossible de créer le service de base de données car « %s » est absent de la configuration',
 		'Unable to create the database service because "%s" does not match any' => 'Impossible de créer le service de base de données car « %s » ne correspond à aucun d\'entre eux',
 
-		// Traduction dans la classe « utils/cache/Cache »
-		'Unable to create "%s" directory' => 'Impossible de créer le dossier « %s »',
-
 		// Traduction dans la classe « utils/Logger »
-		'Unable to create "%s" directory' => 'Impossible de créer le dossier « %s »',
 		'Date' => 'Date',
 		'IP address' => 'Adresse IP',
 		'User agent' => 'Agent utilisateur',
